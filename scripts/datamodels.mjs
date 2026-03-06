a0101c5ad9485aaf86055b70d57cf0bb6a5cad76
Bobby-Jo8555686 <ogame.forever@hotmail.fr>
2026-03-06 19:12:54 +0100

    Update datamodels.mjs

--- scripts/datamodels.mjs
+++ scripts/datamodels.mjs
@@ -37,7 +37,21 @@ const ActivitySchema = new SchemaField({
 export class PersonnageData extends foundry.abstract.TypeDataModel {
     static defineSchema() {
         return {
-            stats: new ObjectField({ initial: {} }),
+            stats: new SchemaField({
+                com: new SchemaField({ value: new NumberField({ initial: 0, integer: true }), label: new StringField({ initial: "Combat" }) }),
+                cns: new SchemaField({ value: new NumberField({ initial: 0, integer: true }), label: new StringField({ initial: "Connaissances" }) }),
+                dis: new SchemaField({ value: new NumberField({ initial: 0, integer: true }), label: new StringField({ initial: "Discrétion" }) }),
+                end: new SchemaField({ value: new NumberField({ initial: 0, integer: true }), label: new StringField({ initial: "Endurance" }) }),
+                for: new SchemaField({ value: new NumberField({ initial: 0, integer: true }), label: new StringField({ initial: "Force" }) }),
+                hab: new SchemaField({ value: new NumberField({ initial: 0, integer: true }), label: new StringField({ initial: "Habileté" }) }),
+                mag: new SchemaField({ value: new NumberField({ initial: 0, integer: true }), label: new StringField({ initial: "Magie" }) }),
+                mou: new SchemaField({ value: new NumberField({ initial: 0, integer: true }), label: new StringField({ initial: "Mouvement" }) }),
+                per: new SchemaField({ value: new NumberField({ initial: 0, integer: true }), label: new StringField({ initial: "Perception" }) }),
+                soc: new SchemaField({ value: new NumberField({ initial: 0, integer: true }), label: new StringField({ initial: "Sociabilité" }) }),
+                sur: new SchemaField({ value: new NumberField({ initial: 0, integer: true }), label: new StringField({ initial: "Survie" }) }),
+                tir: new SchemaField({ value: new NumberField({ initial: 0, integer: true }), label: new StringField({ initial: "Tir" }) }),
+                vol: new SchemaField({ value: new NumberField({ initial: 0, integer: true }), label: new StringField({ initial: "Volonté" }) })
+            }),
             vitalite: new ObjectField({ initial: { value: 0, max: 0 } }),
             sangfroid: new ObjectField({ initial: { value: 0, max: 0 } }),
             destin: new ObjectField({ initial: { value: 0, max: 0 } }),
